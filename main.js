a89caf84a92ed920f688cd046706d012b01c5cfb
井澤伊織 <izawa.iori.tan@gmail.com>
2022-12-31 20:53:56 -0500

    Make the canvas draggable

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 xunit = 0
 yunit = 0
+CLICK_START_MOUSE_POS = [0, 0]
+CMOUSE_POS = [0, 0]
+DELTA = [0, 0]
+DELTA_UPDATED = false
+MOUSE_IS_DOWN = false
 
 function init() { 
     const body = document.getElementsByTagName('body')[0]
@@ -12,13 +17,35 @@ function init() {
     xunit = screen.width * (1/100)
     yunit = screen.height * (1/100)
 
-    drawNode(ctx, 10, 10, 10, 10)
+    nodes = [
+        new DrawNode(10, 10, 10, 10)
+    ]
+
+    update(ctx, nodes, screen)
+}
+
+function update(ctx, nodes, screen) {
+    ctx.clearRect(0, 0, screen.width, screen.height)
+    nodes.forEach(node => { 
+        if (!DELTA_UPDATED) {
+            node.update_position(DELTA)
+        }
+        node.draw(ctx) 
+    })
+    DELTA_UPDATED = true
+    CLICK_START_MOUSE_POS = CMOUSE_POS
+    drawBorder(ctx)
+    requestAnimationFrame(() => {
+        update(ctx, nodes, screen)
+    })
+}
+
+function drawBorder(ctx) {
     ctx.strokeStyle = 'darkslategray';
     ctx.beginPath() ;
     ctx.roundRect(0*xunit, 0*yunit, 100*xunit, 100*yunit, 10)
     ctx.lineWidth = 4
     ctx.stroke()
-    
 }
 
 function drawNode(ctx, x, y, w, h) {
@@ -31,4 +58,44 @@ function drawNode(ctx, x, y, w, h) {
     ctx.fill()
 }
 
+class DrawNode {
+    constructor(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+    }
+
+    draw(ctx) {
+        drawNode(ctx, this.x, this.y, this.w, this.h)
+    }
+
+    update_position(delta) {
+        console.log(delta)
+        this.x -= delta[0]/xunit
+        this.y -= delta[1]/yunit
+    }
+}
+
 init()
+
+document.documentElement.style.cursor = "grabbing"
+
+window.addEventListener("mousedown", e => {
+    CLICK_START_MOUSE_POS = [e.clientX, e.clientY]
+    CMOUSE_POS = [e.clientX, e.clientY]
+    MOUSE_IS_DOWN = true
+    
+})
+
+window.addEventListener("mouseup", _ => {
+    MOUSE_IS_DOWN = false
+})
+
+window.addEventListener("mousemove", e => {
+    if (MOUSE_IS_DOWN) {
+        CMOUSE_POS = [e.clientX, e.clientY]
+        DELTA = [CLICK_START_MOUSE_POS[0] - e.clientX, CLICK_START_MOUSE_POS[1] - e.clientY]
+        DELTA_UPDATED = false
+    } 
+})
